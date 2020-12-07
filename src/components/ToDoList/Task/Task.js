5e224ed7ab9ddd5f3acf9f5be3fcbc5b2bffdc03
David <DavidLegrand@users.noreply.github.com>
2020-12-07 21:47:02 +0100

    Added control buttons in task component

--- src/components/ToDoList/Task/Task.js
+++ src/components/ToDoList/Task/Task.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Badge, ListGroupItem } from "react-bootstrap";
+import { Badge, Button, ListGroupItem } from "react-bootstrap";
 import TaskModel from "models/Task";
 
 const Task = () => {
   const task = new TaskModel({
-    title: "Première tâche"
+    title: "Première tâche",
   });
-  console.log(task)
+  console.log(task);
   return (
     <ListGroupItem variant={task.getVariant()}>
       <h2 className="d-inline">{task.title}</h2>
@@ -17,6 +17,13 @@ const Task = () => {
 
       <div>Echéance : {task.deadline.toLocaleDateString()}</div>
       <div>Temps restant : {task.getRemaining()} jours</div>
+      <div>
+        {task.completed ? (
+          <Button onClick={() => console.log("annuler", task)}>Annuler</Button>
+        ) : (
+          <Button onClick={() => console.log("terminer", task)}>Terminer</Button>
+        )}
+      </div>
     </ListGroupItem>
   );
 };
